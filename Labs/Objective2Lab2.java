398c1681cb061fba61bf420619b4507af1cbe81a
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-07-14 10:14:44 -0400

    complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,10 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Klayton Hutchins";
+    int age = 28;
+    String state = "Ohio";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
