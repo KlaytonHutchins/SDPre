e6d0fe3a288ee64064e85ad2b3d14ac84a206768
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-07-16 09:12:59 -0400

    complete objective8lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,12 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    int c;
+    for(c = 1; c < 11; c++){
+      System.out.println(c);
+    }
+    for(c = 10; c > 0; c--){
+      System.out.println(c);
+    }
+
+  }
+}
