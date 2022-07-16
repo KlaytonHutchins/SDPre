4112c625b462029ff053b96bfb50cc407e9185f6
Klayton Hutchins <klaytonhutchins@gmail.com>
2022-07-16 09:30:56 -0400

    complete objective8lab3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,12 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter;
+    for(counter = 1; counter <= 20; counter++){
+      System.out.print(counter + " is ");
+      if((counter % 2) == 0)
+        System.out.println("even");
+      else
+        System.out.println("odd");
+      }
+  }
+}
